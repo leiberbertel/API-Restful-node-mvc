7519b2234499bc898d4f61af0f4e9f4e41838798
leiber bertel <leiberbertel777@gmail.com>
2023-12-09 23:05:56 -0500

    feat: add the entry point file

---
+++ app.js
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+
+import { createMovieRouter } from './routes/movies.js';
+import { corsMiddleware } from './middlewares/cors.js';
+
+export const createApp = ({ movieModel }) => {
+  const app = express();
+  app.disable('x-powered-by');
+  app.use(json());
+  app.use(corsMiddleware());
+
+  app.use('/movies', createMovieRouter({ movieModel }));
+
+  const PORT = process.env.PORT ?? 1234;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
