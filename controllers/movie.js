002c21070fe20f312038f3e434075a26c3970079
leiber bertel <leiberbertel777@gmail.com>
2023-12-09 23:06:52 -0500

    add controller module

---
+++ controllers/movie.js
@@ -0,0 +1,62 @@
+import { validateMovie, validatePartialMovie } from '../schemas/movie.js';
+
+export class MovieController {
+  constructor ({ movieModel }) {
+    this.movieModel = movieModel;
+  }
+
+  getAll = async (req, res) => {
+    const { genre } = req.query;
+    const movies = await this.movieModel.getAll({ genre });
+
+    res.json(movies);
+  };
+
+  getById = async (req, res) => {
+    const { id } = req.params;
+    const movie = await this.movieModel.getById({ id });
+    if (movie) return res.json(movie);
+
+    res.status(404).json({ message: 'Movie not found' });
+  };
+
+  create = async (req, res) => {
+    const result = validateMovie(req.body);
+
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+
+    const newMovie = await this.movieModel.create({ input: result.data });
+
+    res.status(201).json(newMovie);
+  };
+
+  delete = async (req, res) => {
+    const { id } = req.params;
+
+    const wasDeleted = await this.movieModel.delete({ id });
+
+    if (!wasDeleted) {
+      return res.status(404).json({ message: 'Movie not found' });
+    }
+
+    return res.json({ message: 'Movie deleted' });
+  };
+
+  update = async (req, res) => {
+    const result = validatePartialMovie(req.body);
+
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+
+    const { id } = req.params;
+    const updatedMovie = await this.movieModel.update({ id, input: result.data });
+
+    if (updatedMovie === null) {
+      return res.status(404).json({ message: 'Movie not found' });
+    }
+    return res.json(updatedMovie);
+  };
+}
