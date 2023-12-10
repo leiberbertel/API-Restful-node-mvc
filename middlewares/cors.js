298f6fea908c192764676ae394b6d831cc99b006
leiber bertel <leiberbertel777@gmail.com>
2023-12-09 23:00:13 -0500

    perf: add configurations cors

---
+++ middlewares/cors.js
@@ -0,0 +1,17 @@
+import cors from 'cors';
+
+const ACCEPTED_ORIGINS = [
+  'http://127.0.0.1:5500',
+  'http://127.0.0.1:5600',
+  'http://127.0.0.1:5700'
+];
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+  origin: (origin, callback) => {
+    if (acceptedOrigins.includes(origin) || !origin) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  }
+});
