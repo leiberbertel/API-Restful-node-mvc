c84d9b92a4961fb3a6d4978901ddf8b753787924
leiber bertel <leiberbertel777@gmail.com>
2024-06-02 21:26:12 -0500

    docs: add documentation of methods and classes

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,17 +1,26 @@
-import cors from 'cors';
+import cors from "cors";
 
 const ACCEPTED_ORIGINS = [
-  'http://127.0.0.1:5500',
-  'http://127.0.0.1:5600',
-  'http://127.0.0.1:5700'
+  "http://127.0.0.1:5500",
+  "http://127.0.0.1:5600",
+  "http://127.0.0.1:5700",
 ];
 
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-  origin: (origin, callback) => {
-    if (acceptedOrigins.includes(origin) || !origin) {
-      return callback(null, true);
-    }
+/**
+ * Intercepta la petición y procesa una lista de orígenes aceptados.
+ * @param {Object} [options] Opciones para el middleware.
+ * @param {Array<String>} [options.acceptedOrigins=ACCEPTED_ORIGINS] Orígenes aceptados.
+ * @returns {Function} Middleware de CORS configurado.
+ * @throws {Error} Si el origen de la petición no está permitido.
+ * @author Leiber Bertel
+ */
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+  cors({
+    origin: (origin, callback) => {
+      if (acceptedOrigins.includes(origin) || !origin) {
+        return callback(null, true);
+      }
 
-    return callback(new Error('Not allowed by CORS'));
-  }
-});
+      return callback(new Error("Not allowed by CORS"));
+    },
+  });
--- routes/movies.js
+++ routes/movies.js
@@ -1,16 +1,71 @@
-import { Router } from 'express';
-import { MovieController } from '../controllers/movie.js';
+import { Router } from "express";
+import { MovieController } from "../controllers/movie.js";
 
+/**
+ * Crea un enrutador para manejar las rutas relacionadas con las películas.
+ *
+ * @param {Object} options - Opciones para el enrutador.
+ * @param {Object} options.movieModel - El modelo de datos para las películas.
+ * @returns {Router} El enrutador configurado para las películas.
+ */
 export const createMovieRouter = ({ movieModel }) => {
   const moviesRouter = Router();
   const movieController = new MovieController({ movieModel });
 
-  moviesRouter.get('/', movieController.getAll);
-  moviesRouter.post('/', movieController.create);
+  /**
+   * Ruta para obtener todas las películas.
+   * @name get/
+   * @function
+   * @memberof module:routers/moviesRouter
+   * @inner
+   * @param {Request} req - Objeto de solicitud.
+   * @param {Response} res - Objeto de respuesta.
+   */
+  moviesRouter.get("/", movieController.getAll);
 
-  moviesRouter.get('/:id', movieController.getById);
-  moviesRouter.delete('/:id', movieController.delete);
-  moviesRouter.patch('/:id', movieController.update);
+  /**
+   * Ruta para crear una nueva película.
+   * @name post/
+   * @function
+   * @memberof module:routers/moviesRouter
+   * @inner
+   * @param {Request} req - Objeto de solicitud.
+   * @param {Response} res - Objeto de respuesta.
+   */
+  moviesRouter.post("/", movieController.create);
+
+  /**
+   * Ruta para obtener una película por su ID.
+   * @name get/:id
+   * @function
+   * @memberof module:routers/moviesRouter
+   * @inner
+   * @param {Request} req - Objeto de solicitud.
+   * @param {Response} res - Objeto de respuesta.
+   */
+  moviesRouter.get("/:id", movieController.getById);
+
+  /**
+   * Ruta para eliminar una película por su ID.
+   * @name delete/:id
+   * @function
+   * @memberof module:routers/moviesRouter
+   * @inner
+   * @param {Request} req - Objeto de solicitud.
+   * @param {Response} res - Objeto de respuesta.
+   */
+  moviesRouter.delete("/:id", movieController.delete);
+
+  /**
+   * Ruta para actualizar parcialmente una película por su ID.
+   * @name patch/:id
+   * @function
+   * @memberof module:routers/moviesRouter
+   * @inner
+   * @param {Request} req - Objeto de solicitud.
+   * @param {Response} res - Objeto de respuesta.
+   */
+  moviesRouter.patch("/:id", movieController.update);
 
   return moviesRouter;
 };
